47e0d290afe5a00cbe44ef3b70e5908f34196676
Levi Nichols <leenich@hotmail.com>
2019-03-15 16:41:19 -0500

    Finished Text 101 commit

--- Text101/Assets/Scripts/AdventureGame.cs
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -28,19 +28,51 @@ public class AdventureGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            state = nextStates[0];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            state = nextStates[1];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            state = nextStates[2];
-        }
 
-        textComponent.text = state.GetStateStory();
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i) || (Input.GetKeyDown(KeyCode.Keypad1 + i)))
+            {
+                state = nextStates[i];
+
+                textComponent.text = state.GetStateStory();
+            }
+            //else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Escape))
+            //{
+                //For closing out Windows Application
+                //Application.Quit();
+
+                //For ending runtime in Unity Editor
+                //UnityEditor.EditorApplication.isPlaying = false;
+            //}
+        }
     }
+
+    //private void ManageState()
+    //{
+    //    var nextState = state.GetNextStates();
+
+    //    if (Input.GetKeyDown(KeyCode.Alpha1) || (Input.GetKeyDown(KeyCode.Keypad1)))
+    //    {
+    //        state = nextState[0];
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.Alpha2) || (Input.GetKeyDown(KeyCode.Keypad2)))
+    //    {
+    //        state = nextState[1];
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.Alpha3) || (Input.GetKeyDown(KeyCode.Keypad3)))
+    //    {
+    //        state = nextState[2];
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.Q) || (Input.GetKeyDown(KeyCode.Escape)))
+    //    {
+    //        //For closing out Windows Application
+    //        //Application.Quit();
+
+    //        //For ending runtime in Unity Editor
+    //        UnityEditor.EditorApplication.isPlaying = false;
+    //    }
+
+    //    textComponent.text = state.GetStateStory();
+    //}
 }
